ff26e2972713403cbe33ca6c31bfa49e698da56e
Skhrebtan <shrebtan@gmail.com>
2024-04-06 14:17:14 +0300

    use client

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,17 +3,20 @@ import initTranslations from "@/app/i18n";
 import { getAllDogs } from "../api/httprequests";
 import DogsList from "../components/DogsList";
 import { getServerSession } from "next-auth";
+import { useTranslation } from "react-i18next";
+import { useSession } from "next-auth/react";
 type HomeProps = {
   params: {
     locale: string;
   };
 }
 
-export default async function Home({ params }: HomeProps) {
-  const session = await getServerSession();
+export default function Home({ params }: HomeProps) {
+  const session =  useSession();
 
-  const { t } = await initTranslations(params.locale, ['common']) 
-  const dogs = await getAllDogs()
+  const { t } = useTranslation();
+  // const dogs = await getAllDogs()
+  const dogs = [{id: '1', name:'tyzlo', breed: 'dvir', image:'http://res.cloudinary.com/dwzeqka9z/image/upload/v1712308074/ajiwndkzlzvwfxcjjqpv.jpg'}]
   return (    
       <main>
       <h1>{t('hello')}</h1>
