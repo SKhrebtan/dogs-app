2278639deb609266974aa36f70f45b4656bb4537
Skhrebtan <shrebtan@gmail.com>
2024-04-06 14:06:18 +0300

    123

--- middleware.js
+++ middleware.js
@@ -1,10 +1,16 @@
 import { i18nRouter } from "next-i18n-router";
 import i18nConfig from "./i18nConfig";
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request) {
-    return i18nRouter(request, i18nConfig)
+export async function middleware(request) {
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+  return i18nRouter(request, i18nConfig);
 }
 
 export const config = {
-    matcher: '/((?!api|static|.*\\..*|_next).*)'
-}
+  matcher: "/((?!api|static|.*\\..*|_next).*)",
+};
