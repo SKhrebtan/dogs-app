e794d48fb4c00867e0587aa004b27754de2688b9
Skhrebtan <shrebtan@gmail.com>
2024-04-07 00:22:52 +0300

    1234

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,7 +3,6 @@ import initTranslations from "@/app/i18n";
 import { getAllDogs } from "../api/httprequests";
 import DogsList from "../components/DogsList";
 import { getServerSession } from "next-auth";
-import { auth } from "../api/auth/[...nextauth]/route";
 type HomeProps = {
   params: {
     locale: string;
@@ -12,9 +11,8 @@ type HomeProps = {
 import authOptions from "../api/auth/authOptions";
 export default async function Home({ params }: HomeProps) {
   const session = await getServerSession(authOptions);
-  // const auth1 = await auth()
   console.log(session)
-  console.log(auth)
+
   const { t } = await initTranslations(params.locale, ['common']) 
   const dogs = await getAllDogs()
   return (    
