0b2dda0cc6f063010733e10c26a8544a7d9e524f
Skhrebtan <shrebtan@gmail.com>
2024-04-08 21:33:42 +0300

    234

--- app/[locale]/mydogs/[...id]/page.tsx
+++ app/[locale]/mydogs/[...id]/page.tsx
@@ -1,20 +1,20 @@
 'use client'
 import { useGetOneDogQuery } from "@/app/store/dogs/dogsSlice"
 import DogDetailsPage from '../../../components/DogDetailsPage'
-import { notFound} from "next/navigation";
+import { notFound } from "next/navigation";
+import { useEffect } from "react";
 interface Params {
   id: string;
 }
 function OneDogPage({ params:{id} }:{params:Params}) {
   const { data, isLoading, error } = useGetOneDogQuery(id[0]);
 
-   if (error && error?.data.statusCode !== 404) {    
-    return notFound()
-    }
+
+ 
   return (
     <div>
       {data && <DogDetailsPage dog={data} />}
-      {error && error.data.statusCode === 404 && <h1>{error.data.message}</h1>}
+      {/* {error && error.data && error.data.statusCode === 404 && <h1>{error.data.message}</h1>} */}
 
     </div>
     )
