d42c6c832d6129fb2aee415ca4c7a1252e0a3ba1
Skhrebtan <shrebtan@gmail.com>
2024-04-08 08:52:52 +0300

    not found styles

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link'
  
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found- 404</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex flex-col items-center justify-center h-screen gap-[30px]">
+      <h2 className="text-6xl font-bold text-gray-900">Not Found- 404</h2>
+      <p className="text-2xl text-gray-600">Could not find requested resource</p>
+      <Link href="/" className='p-[10px] bg-slate-400 rounded-lg hover:bg-slate-700 ease-linear transition 300'>Return Home</Link>
     </div>
   )
 }
