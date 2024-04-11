81a762801809f0a86dfa7a5dc6cef1261296b6b4
Skhrebtan <shrebtan@gmail.com>
2024-04-11 13:32:26 +0300

    fix

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,19 +1,12 @@
-
-
-import { getAllDogs } from "../api/httprequests";
 import DogsList from "../components/DogsList";
 import { getServerSession } from "next-auth";
-import authOptions from "../api/auth/authOptions";;
+import authOptions from "../api/auth/authOptions";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-  // const dogs = await getAllDogs()
-  return (   
-      <main>
-        <DogsList dogs={[]} page='home' />
-      </main>
-
+  // const session = await getServerSession(authOptions);
+  return (
+    <main>
+      <DogsList dogs={[]} page="home" />
+    </main>
   );
 }
-
-
